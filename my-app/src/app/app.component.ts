f2e0e84ce03dec63e9be4882d0b1781f40468f2e
nikhil <nikhildobariya812@gmail.com>
2023-07-14 18:22:51 +0530

    Made changes to the program

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validator, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +7,76 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  loginForm = new FormGroup({
+    name:new FormControl('',[Validators.required, Validators.pattern('[a-zA-z]+$')]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(6)]),
+    phoneNumber:new FormControl('',[Validators.required,Validators.maxLength(15), Validators.pattern('[0-9]+$')]),
+    gender:new FormControl('male',[Validators.required]),
+    dob: new FormControl('', [Validators.required, this.validateDateOfBirth]),
+    city:new FormControl('',[Validators.required]),
+    hobby:new FormControl('',[Validators.required]),
+    agree:new FormControl('',[Validators.required]),
+
+  }) 
+
+  loginUser(){
+    console.warn(this.loginForm.value);
+  }
+
+ get name(){
+    return this.loginForm.get('name')
+  }
+
+  get password(){
+    return this.loginForm.get('password')
+  }
+  
+  get email(){
+    return this.loginForm.get('email')
+  }
+  get gender() {
+    return this.loginForm.get('gender');
+  }
+  
+  get dob() {
+    return this.loginForm.get('dob');
+  }
+  
+  get city() {
+    return this.loginForm.get('city');
+  }
+  
+  get agree() {
+    return this.loginForm.get('agree');
+  }
+ 
+  get phoneNumber() {
+    return this.loginForm.get('phoneNumber');
 
 }
+  get hobby() {
+    return this.loginForm.get('hobby');
+}
+
+
+validateDateOfBirth(control: FormControl): { [key: string]: any } | null {
+  const dateOfBirth = new Date(control.value);
+  const age = this.calculateAge(dateOfBirth);
+
+  if (isNaN(dateOfBirth.getTime()) || age < 18) {
+    return { 'invalidDateOfBirth': true };
+  }
+
+  return null;
+}
+
+calculateAge(birthday: Date) {
+  const ageDifMs = Date.now() - birthday.getTime();
+  const ageDate = new Date(ageDifMs);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+
+}
+
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestingComponent } from './testing/testing.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestingComponent
+    
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,ReactiveFormsModule,
     AppRoutingModule,
     NgbModule
   ],
